5aeba020faecbf70fc3aea7d1725d4fb69e9868a
SALMAN <SALMAN@DESKTOP-Q0BOMET>
2022-08-28 21:22:27 +0500

    Add project files.

---
+++ whileloop/Program.cs
@@ -0,0 +1,49 @@
+﻿double fvalue, svalue;
+char choice, oprator;
+
+
+
+Console.WriteLine("Did You Want to use calculator?"+Environment.NewLine + "Press Y to Continue or press any key to close");
+choice = Console.ReadKey().KeyChar;
+
+while (choice == 'y' || choice == 'Y')
+{
+	Console.WriteLine("enter first value");
+	fvalue = Convert.ToDouble(Console.ReadLine());
+
+	Console.WriteLine("enter operator");
+	oprator = Console.ReadKey().KeyChar;
+	Console.WriteLine();
+
+Console.WriteLine("enter second value");
+	svalue = Convert.ToDouble(Console.ReadLine());
+
+	if(oprator=='+')
+	{
+		Console.WriteLine("Reault:"+Convert.ToDouble(fvalue+svalue));
+	}
+
+	else if (oprator == '-')
+	{
+		Console.WriteLine("Reault:" + Convert.ToDouble(fvalue-svalue));
+	}
+
+	else if (oprator == '*')
+	{
+		Console.WriteLine("Reault:" + Convert.ToDouble(fvalue*svalue));
+	}
+
+	else  if (oprator == '/')
+	{
+		Console.WriteLine("Reault:" + Convert.ToDouble(fvalue/svalue));
+	}
+
+	else
+	{
+		Console.WriteLine("Wrong Command");
+	}
+
+	Console.WriteLine("Did You Want to use calculator?" + Environment.NewLine + "Press Y to Continue or press any key to close");
+	choice = Console.ReadKey().KeyChar;
+}
+Console.WriteLine("Closed");
